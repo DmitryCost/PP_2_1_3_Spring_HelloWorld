ca11997388fc27546e243813885a8d6c0fe7cccc
DmitryCost <70369805+DmitryCost@users.noreply.github.com>
2023-02-15 21:51:33 +0700

    finish

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = applicationContext.getBean(HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
+
+        Cat beanCat = applicationContext.getBean(Cat.class);
+        Cat beanCat2 = applicationContext.getBean(Cat.class);
+        System.out.println("First " + (bean1 == bean2));
+        System.out.println("Second " + (beanCat == beanCat2));
+
     }
 }
